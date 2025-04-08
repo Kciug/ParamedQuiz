1ef40753ae4978c0f16a83022cbb67ce90d8d195
RafalS <rafalskrzypczyk96@gmail.com>
2025-04-08 19:08:18 +0200

    google services setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
-    //id("com.google.gms.google-services")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
 
     id("kotlin-kapt")
 }
