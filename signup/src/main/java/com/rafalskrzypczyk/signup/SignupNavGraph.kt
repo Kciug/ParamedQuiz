23a1bf5d5dcb884f1d31a7f780d4ac33adbbec7c
RafalS <rafalskrzypczyk96@gmail.com>
2025-04-11 22:19:06 +0200

    module setup

---
+++ signup/src/main/java/com/rafalskrzypczyk/signup/SignupNavGraph.kt
@@ -0,0 +1,15 @@
+package com.rafalskrzypczyk.signup
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import kotlinx.serialization.Serializable
+
+@Serializable
+object SignupNav
+
+fun NavGraphBuilder.signupNavGraph(navController: NavHostController) {
+//    navigation<SignupNav>(startDestination = Registration) {
+//
+//    }
+}
+
