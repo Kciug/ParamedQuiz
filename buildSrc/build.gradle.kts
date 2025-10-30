d29d318f2089e0915b788b30fe4d1c511ac80d84
RafalS <rafalskrzypczyk96@gmail.com>
2025-10-30 14:30:10 +0100

    gradle optimization for release adjustments

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
-    implementation("com.android.tools.build:gradle:8.7.2")
+    implementation("com.android.tools.build:gradle:8.13.0")
     implementation("org.jetbrains.kotlin.plugin.compose:org.jetbrains.kotlin.plugin.compose.gradle.plugin:2.1.20")
     implementation("com.squareup:javapoet:1.13.0")
 }
