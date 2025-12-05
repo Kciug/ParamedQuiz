58e6d0c6c5d2b35bafbda8a3ec767d6048fd2d31
RafalS <rafalskrzypczyk96@gmail.com>
2025-12-05 21:54:39 +0100

    github actions workflows adjustments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,21 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            val keystoreFile = file("keystore.jks")
+            if (keystoreFile.exists()) {
+                storeFile = keystoreFile
+                storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+                keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+                keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+            }
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
 
             isShrinkResources = true
