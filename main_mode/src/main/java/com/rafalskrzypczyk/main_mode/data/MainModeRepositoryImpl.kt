6cf9215db2786294d21c9c82c9e105278112d8af
RafalS <rafalskrzypczyk96@gmail.com>
2025-12-05 19:50:29 +0100

    caching adjustments

--- main_mode/src/main/java/com/rafalskrzypczyk/main_mode/data/MainModeRepositoryImpl.kt
+++ main_mode/src/main/java/com/rafalskrzypczyk/main_mode/data/MainModeRepositoryImpl.kt
@@ -8,6 +8,8 @@ import com.rafalskrzypczyk.main_mode.domain.models.Question
 import com.rafalskrzypczyk.main_mode.domain.models.toDomain
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -18,37 +20,53 @@ class MainModeRepositoryImpl @Inject constructor(
     private var categories: List<Category>? = null
     private var questions: List<Question>? = null
 
-    override fun getAllCategories(): Flow<Response<List<Category>>> = firestore.getQuizCategories().map {
-            when (it) {
-                is Response.Success -> {
-                    categories = it.data.map { it.toDomain() }
-                   Response.Success(categories!!)
+    override fun getAllCategories(): Flow<Response<List<Category>>> = flow {
+        if (!categories.isNullOrEmpty()) {
+            emit(Response.Success(categories!!))
+        } else {
+            emitAll(
+                firestore.getQuizCategories().map {
+                    when (it) {
+                        is Response.Success -> {
+                            categories = it.data.map { dto -> dto.toDomain() }
+                            Response.Success(categories!!)
+                        }
+                        is Response.Error -> Response.Error(it.error)
+                        is Response.Loading -> Response.Loading
+                    }
                 }
-                is Response.Error -> Response.Error(it.error)
-                is Response.Loading -> Response.Loading
-            }
-        }.flowOn(Dispatchers.Default)
+            )
+        }
+    }.flowOn(Dispatchers.Default)
 
 
-    override fun getAllQuestions(): Flow<Response<List<Question>>> = firestore.getQuizQuestions().map {
-            when (it) {
-                is Response.Success -> {
-                    questions = it.data.map { it.toDomain() }
-                    Response.Success(questions!!)
+    override fun getAllQuestions(): Flow<Response<List<Question>>> = flow {
+        if (!questions.isNullOrEmpty()) {
+            emit(Response.Success(questions!!))
+        } else {
+            emitAll(
+                firestore.getQuizQuestions().map {
+                    when (it) {
+                        is Response.Success -> {
+                            questions = it.data.map { dto -> dto.toDomain() }
+                            Response.Success(questions!!)
+                        }
+                        is Response.Error -> Response.Error(it.error)
+                        is Response.Loading -> Response.Loading
+                    }
                 }
-                is Response.Error -> Response.Error(it.error)
-                is Response.Loading -> Response.Loading
-            }
-        }.flowOn(Dispatchers.Default)
+            )
+        }
+    }.flowOn(Dispatchers.Default)
 
 
     override fun getUpdatedCategories(): Flow<List<Category>> = firestore.getUpdatedCategories().map {
-        categories = it.map { it.toDomain() }
+        categories = it.map { dto -> dto.toDomain() }
         categories!!
     }.flowOn(Dispatchers.Default)
 
     override fun getUpdatedQuestions(): Flow<List<Question>> = firestore.getUpdatedQuestions().map {
-        questions = it.map { it.toDomain() }
+        questions = it.map { dto -> dto.toDomain() }
         questions!!
     }.flowOn(Dispatchers.Default)
 }
--- swipe_mode/src/main/java/com/rafalskrzypczyk/swipe_mode/data/SwipeModeRepositoryImpl.kt
+++ swipe_mode/src/main/java/com/rafalskrzypczyk/swipe_mode/data/SwipeModeRepositoryImpl.kt
@@ -7,6 +7,8 @@ import com.rafalskrzypczyk.swipe_mode.domain.SwipeQuestion
 import com.rafalskrzypczyk.swipe_mode.domain.toDomain
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -17,19 +19,27 @@ class SwipeModeRepositoryImpl @Inject constructor(
 ): SwipeModeRepository {
     private var swipeQuestions: List<SwipeQuestion>? = null
 
-    override fun getSwipeQuestions(): Flow<Response<List<SwipeQuestion>>> = firestore.getSwipeQuestions().map {
-        when (it) {
-            is Response.Success -> {
-                swipeQuestions = it.data.map { it.toDomain() }
-                Response.Success(swipeQuestions!!)
-            }
-            is Response.Error -> Response.Error(it.error)
-            is Response.Loading -> Response.Loading
+    override fun getSwipeQuestions(): Flow<Response<List<SwipeQuestion>>> = flow {
+        if (!swipeQuestions.isNullOrEmpty()) {
+            emit(Response.Success(swipeQuestions!!))
+        } else {
+            emitAll(
+                firestore.getSwipeQuestions().map {
+                    when (it) {
+                        is Response.Success -> {
+                            swipeQuestions = it.data.map { dto -> dto.toDomain() }
+                            Response.Success(swipeQuestions!!)
+                        }
+                        is Response.Error -> Response.Error(it.error)
+                        is Response.Loading -> Response.Loading
+                    }
+                }
+            )
         }
     }.flowOn(Dispatchers.Default)
 
     override fun getUpdatedQuestions(): Flow<List<SwipeQuestion>> = firestore.getUpdatedSwipeQuestions().map {
-        swipeQuestions = it.map { it.toDomain() }
+        swipeQuestions = it.map { dto -> dto.toDomain() }
         swipeQuestions!!
     }.flowOn(Dispatchers.Default)
 }
