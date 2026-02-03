a1c0b20be40a9fa38543c3cb5af6cfbe72d61ce7
RafalS <rafalskrzypczyk96@gmail.com>
2026-02-04 00:52:10 +0100

    ReportIssueDialog.kt - imports handling

--- core/src/main/java/com/rafalskrzypczyk/core/composables/ReportIssueDialog.kt
+++ core/src/main/java/com/rafalskrzypczyk/core/composables/ReportIssueDialog.kt
@@ -6,14 +6,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.OutlinedFlag
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
