f1c67c3936fce9856a05d0fa53130d30d2b9d81f
RafalS <rafalskrzypczyk96@gmail.com>
2026-03-29 18:33:40 +0200

    Bump VERSION_CODE to 106
    - Incremented `VERSION_CODE` from 105 to 106 in `ProjectConfig.kt`.

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -3,6 +3,6 @@ object ProjectConfig {
     const val MIN_SDK = 24
     const val TARGET_SDK = 36
     const val COMPILE_SDK = 36
-    const val VERSION_CODE = 105
+    const val VERSION_CODE = 106
     const val VERSION_NAME = "1.0.0"
 }
